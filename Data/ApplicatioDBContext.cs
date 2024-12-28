cc2c905e2ae42bbad24334b601fba8e7f071807a
Abhishek Shishodia <singh.abhishek140@gmail.com>
2024-12-28 17:47:26 +0530

    Added DbContext,Models,etc.

---
+++ Data/ApplicatioDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeApp.Models;
+
+namespace WeApp.Data
+{
+    public class ApplicatioDBContext : DbContext
+    {
+        public ApplicatioDBContext(DbContextOptions<ApplicatioDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
